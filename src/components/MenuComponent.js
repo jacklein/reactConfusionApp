56049ca76b6a3c25d8d885ae2f5205b1acccc995
Jack K <j.klein159@yahoo.com>
2018-08-28 19:36:21 -0500

    steady progress on first react app

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Menu extends Component {
+
+    constructor(props) {
+        // Menu is inheriting from React.Component by calling super
+        // you are calling the parent element with props parameter
+        super(props);
+
+        this.state = { //instance of object
+            
+        }
+    }
+
+    render() {
+
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card tag="li">
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />                      
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );       
+        });
+
+        return ( 
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
